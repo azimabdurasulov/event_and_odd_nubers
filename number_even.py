76b73385f128b300148c4551ec954a26c73988dd
azimabdurasulov <azimjon20050604@gmail.com>
2022-09-24 10:03:38 +0500

    3-vazifa

--- number_even.py
+++ number_even.py
@@ -1,5 +1,24 @@
 #A four-digit integer is given. Find the number of even digits in it.
 
 #Create a variable "var_int" and assign it a four-digit integer value.
+var_int = 2468
 
+counter = 0
+
+x1 = var_int % 10 + 1
+counter += x1 % 2
+var_int //= 10
+
+x2 = var_int % 10 + 1
+counter += x2 % 2
+var_int //= 10
+
+x3 = var_int % 10 + 1
+counter += x3 % 2
+var_int //= 10
+
+x4 = var_int % 10 + 1
+counter += x4 % 2
+var_int //= 10
 #Print the number of even digits in the variable "var_int".
+print(counter)
